9365dc650fe3accc7718b43078a857a2b01d110e
Wasi Genius <wasigeniuswg@gmail.com>
2025-06-06 10:09:20 -0400

    Trying to connect database

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 console.log(process.env.MONGO_URI);
 
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+    xonnectMongoDB();
 });
