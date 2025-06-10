81937171291a968b84d84fbeea72d4315416939c
Wasi Genius <wasigeniuswg@gmail.com>
2025-06-10 08:02:47 -0400

    trying to run database test

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express"; 
-import dotenv from "dotenv";
+import dotenv from "dotenv";``
 
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
@@ -14,5 +15,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
-    xonnectMongoDB();
+    connectMongoDB();
 });
