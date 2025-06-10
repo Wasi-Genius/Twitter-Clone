07ba2a5f6f944eef575631b7b01b4e4cf4cb2004
Wasi Genius <wasigeniuswg@gmail.com>
2025-06-10 09:41:49 -0400

    setting up user auth

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,26 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
-    res.json({
-        data: "You hit the signup endpoint",
-    });
+   try {
+     const {fullName, username, email, password} = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({
+            error: "Invalid email format",
+        });
+    }
+
+    const existingUser = await User.find({ username})
+    if (existingUser) {
+        return res.status(400).json({
+            error: "Username already exists",
+        });
+    }
+
+   } catch (error) {
+    
+   }
 }
 
 export const login = async (req, res) => {
