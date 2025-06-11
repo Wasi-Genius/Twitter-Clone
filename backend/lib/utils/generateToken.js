91eaac9d3c8f368ce42b83290f5cedd4ed85dee1
Wasi Genius <wasigeniuswg@gmail.com>
2025-06-11 07:45:30 -0400

    Client side auth cookie created.

---
+++ backend/lib/utils/generateToken.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export const generateTokernAndSetCookie = (userId, res) => {
+    const token = jwt.sign({userId},process.env.JWT_SECRET, {
+        expiresIn: '15d',
+    });
+    res.cookie('jwt', token, {
+        httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+        secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+        sameSite: 'strict', // Helps prevent CSRF attacks
+        maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
+    });
+}
