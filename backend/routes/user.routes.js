c5e62ff4922b822f40adcc1d9e6da10270ece1f3
Wasi Genius <wasigeniuswg@gmail.com>
2025-06-29 19:47:25 -0400

    created user routes

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { protectRoute } from '../middleware/protectRoute.js';
+
+const router = express.Router();
+
+router.get("/profile/:username", getUserProfile)
+    
+router.get("/suggested", protectRoute, getUserProfile)
+
+router.post("/follow/:id", protectRoute, followUnfollowUser)
+
+router.post("/update", protectRoute, updateUserProfile)
+
+export default router; 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
@@ -10,12 +12,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // to parse req.body (the form data from the user)
-app.use(express.urlencoded({ extended: true })); // to parse urlencoded data
+app.use(express.json()); 
+
+app.use(express.urlencoded({ extended: true })); 
 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
