896af51dee02e97e605e0016910216a1bf92d480
Wasi Genius <wasigeniuswg@gmail.com>
2025-07-03 07:46:37 -0400

    post functionality completed

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Post from "../models/post.model.js";
+import Notification from "../models/notification.model.js";
 import { v2 as cloudinary } from "cloudinary";
 
 export const createPost = async (req, res) => {
@@ -69,5 +70,99 @@ export const deletePost = async (req, res) => {
             message: "Internal server error!",
             error: error.message
         });
+    }
+};
+
+export const commentOnPost = async (req, res) => {
+
+    try {
+        const {text} = req.body; 
+        const postId = req.params.id; 
+        const userId = req.user._id; 
+
+        if(!text){
+            return res.status(400).json({message: "Comment text is required"});
+        }
+
+        const post = await Post.findById(postId)
+
+        if(!post){
+            return res.status(404).json({message: "Post not found"});
+        }
+
+        const comment = {
+            text,
+            user: userId
+        };
+
+        post.comments.push(comment); 
+
+        await post.save();
+
+        res.status(201).json({
+            message: "Comment added successfully",
+            comment
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error!",
+            error: error.message
+        });
+    }
+}
+
+export const likeUnlikePost = async (req, res) => {
+    try {
+        
+        const postId = req.params.id; 
+        const userId = req.user._id; 
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({message: "Post not found"});
+        }
+
+        if (post.likes.includes(userId)) {
+            //Unlike the post
+            await Post.updateOne({_id:postId}, {$pull: {likes: userId}});
+            res.status(200).json({
+                message: "Post unliked successfully",
+            });
+
+        } else {
+            post.likes.push(userId);
+            await post.save();
+
+            const notification = new Notification({
+                from: userId, 
+                to: post.user,
+                type: "like"
+            })
+
+            await notification.save();
+
+            res.status(200).json({
+                message: "Post liked successfully",
+                likes: post.likes
+            });
+        }
+
+        await post.save();
+
+        res.status(200).json({
+            message: "Post liked/unliked successfully",
+            likes: post.likes
+        });
+
+
+    } catch (error) {
+
+        return res.status(500).json({
+            message: "Internal server error!",
+            error: error.message
+        });
+        
     }
 }
--- backend/models/notification.model.js
+++ backend/models/notification.model.js
@@ -13,7 +13,7 @@ const notificationSchema = new mongoose.Schema({
     },
     type: {
         type: String,
-        enum: ["follow", "like", "comment", "retweet"],
+        enum: ["follow", "like"],
         required: true
     },
     read: {
