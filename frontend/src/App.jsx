ef4a3b77da18c461d044858642174852fce064d6
Wasi Genius <wasigeniuswg@gmail.com>
2025-07-07 08:59:04 -0400

    trying to set up the sign up page front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import {Route, Routes} from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import LoginPage from './pages/auth/login/LoginPage';
+
 
 function App() {
-  return (
-    <>
-      <p>Hello world!</p>
-      <button className="btn btn-secondary">Secondary</button>
-    </>
-  )
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App
