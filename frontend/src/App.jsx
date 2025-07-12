b62d53fbd3725db9f544e1cdcdc5814755de31b6
Wasi Genius <wasigeniuswg@gmail.com>
2025-07-12 09:52:17 -0400

    Finished setting up the home page front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,21 @@ import {Route, Routes} from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
 import LoginPage from './pages/auth/login/LoginPage';
+import SideBar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+import RightPanelSkeleton from './components/skeletons/RightPanelSkeleton';
 
 
 function App() {
 	return (
 		<div className='flex max-w-6xl mx-auto'>
+			<SideBar/>
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignUpPage />} />
 				<Route path='/login' element={<LoginPage />} />
 			</Routes>
+			<RightPanel />
 		</div>
 	);
 }
