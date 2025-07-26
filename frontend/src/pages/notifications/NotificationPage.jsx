03819a2c0094f047b2039f61ad1deb49a895efb8
Wasi Genius <wasigeniuswg@gmail.com>
2025-07-26 13:53:38 -0400

    added commenting to posts

--- frontend/src/pages/notifications/NotificationPage.jsx
+++ frontend/src/pages/notifications/NotificationPage.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import {useQuery, useMutation} from "@tanstack/react-query";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
 
 import { IoSettingsOutline } from "react-icons/io5";
@@ -6,27 +7,6 @@ import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
 
 	const deleteNotifications = () => {
 		alert("All notifications deleted");
