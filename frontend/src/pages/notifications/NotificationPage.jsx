bb3c51a422899b1c8f2b1bcf7832b84e2fe1b3b1
Wasi Genius <wasigeniuswg@gmail.com>
2025-07-30 14:01:11 -0400

    notifications not working

--- frontend/src/pages/notifications/NotificationPage.jsx
+++ frontend/src/pages/notifications/NotificationPage.jsx
@@ -10,8 +10,9 @@ import { toast } from "react-toastify";
 
 const NotificationPage = () => {
 
-	const {data: notifications, isLoading} = useQuery({
-		const queryClient = useQueryClient();
+	const queryClient = useQueryClient()
+
+	const {data: notifications, isLoading} = useQueryClient({
 		queryKey: ["notifications"],
 		queryFn: async () => {
 			try {
@@ -31,6 +32,8 @@ const NotificationPage = () => {
 		
 		},
 	})
+
+	
 
 	const {mutate: deleteNotifications} = useMutation({
 		mutationFn: async () => {
@@ -83,7 +86,11 @@ const NotificationPage = () => {
 						<LoadingSpinner size='lg' />
 					</div>
 				)}
+				
 				{notifications?.length === 0 && <div className='text-center p-4 font-bold'>No notifications 🤔</div>}
+				
+				{console.log("Notifications value:", notifications)}
+
 				{notifications?.map((notification) => (
 					<div className='border-b border-gray-700' key={notification._id}>
 						<div className='flex gap-2 p-4'>
