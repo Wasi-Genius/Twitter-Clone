f350ffa1b0aa70bcb2ac06a57b7f8dd97d1c927c
Wasi Genius <wasigeniuswg@gmail.com>
2026-05-22 14:05:48 -0400

    fixed files that should be ignored and got website running again

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
 	try {
+		// ADD THESE TWO LOGS:
+        console.log("--- DEBUGGING ENV VARIABLES ---");
+        console.log("What Node sees for MONGO_URI:", process.env.MONGO_URI);
 		const conn = await mongoose.connect(process.env.MONGO_URI);
 	} catch (error) {
 		console.error("Error connecting to MongoDB:", error.message);
